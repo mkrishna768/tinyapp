db974002021846e50e7873672406745caba26be2
Michael Krishna <michaelkrishna@hotmail.com>
2020-11-10 23:25:15 +0000

    handling errors in registration

--- express_server.js
+++ express_server.js
@@ -13,6 +13,14 @@ function generateRandomString() {
   return string;
 };
 
+function emailLookUp(email) {
+  for (let user in users) {
+    if (user.email === email) {
+      return user.id;
+    }
+  }
+}
+
 app.set("view engine", "ejs");
 const bodyParser = require("body-parser");
 
@@ -46,7 +54,7 @@ app.get("/urls.json", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  const templateVars = { username: req.cookies["username"], urls: urlDatabase };
+  const templateVars = { user: users[req.cookies["user_id"]], urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
 
@@ -57,14 +65,26 @@ app.post("/urls", (req, res) => {
 });
 
 app.get("/register", (req, res) => {
-  const templateVars = { username: req.cookies["username"] };
+  const templateVars = { user: users[req.cookies["user_id"]] };
   res.render("register", templateVars);
 });
 
-app.post("/urls", (req, res) => {
-  const id = generateRandomString();
-  urlDatabase[id] = req.body.longURL;
-  res.redirect(`/urls/${id}`);
+app.post("/register", (req, res) => {
+  if (req.body.email === "" || req.body.password === "") {
+    res.status(400).send("Invalid email or password");
+  } else if (!emailLookUp(req.body.email)) {
+    res.status(400).send("Email already registered");
+  } else {
+    const id = generateRandomString();
+    users[id] = {
+      id,
+      email: req.body.email,
+      password: req.body.password
+    };
+    res.cookie('user_id', id);
+    console.log(users);
+    res.redirect('/urls');
+  }
 });
 
 app.post("/login", (req, res) => {
@@ -78,7 +98,7 @@ app.post("/logout", (req, res) => {
 });
 
 app.get("/urls/new", (req, res) => {
-  const templateVars = { username: req.cookies["username"] };
+  const templateVars = { user: users[req.cookies["user_id"]] };
   res.render("urls_new", templateVars);
 });
 
@@ -93,7 +113,7 @@ app.post("/urls/:shortURL/delete", (req, res) => {
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { username: req.cookies["username"], shortURL: req.params.shortURL, longURL: req.params.longURL };
+  const templateVars = { user: users[req.cookies["user_id"]], shortURL: req.params.shortURL, longURL: req.params.longURL };
   res.render("urls_show", templateVars);
 });
 
