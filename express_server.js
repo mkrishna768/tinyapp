0c9643099401eb980bcac85f1e279818922078d5
Michael Krishna <michaelkrishna@hotmail.com>
2020-11-10 21:20:46 +0000

    Username stored in cookie after login and displayed on navbar

--- express_server.js
+++ express_server.js
@@ -1,5 +1,7 @@
 const express = require("express");
+const cookieParser = require('cookie-parser')
 const app = express();
+app.use(cookieParser());
 const PORT = 8080; // default port 8080
 
 function generateRandomString() {
@@ -13,6 +15,8 @@ function generateRandomString() {
 
 app.set("view engine", "ejs");
 const bodyParser = require("body-parser");
+
+
 app.use(bodyParser.urlencoded({extended: true}));
 
 const urlDatabase = {
@@ -29,7 +33,7 @@ app.get("/urls.json", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = { username: req.cookies["username"], urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
 
@@ -39,8 +43,14 @@ app.post("/urls", (req, res) => {
   res.redirect(`/urls/${id}`);
 });
 
+app.post("/login", (req, res) => {
+  res.cookie('username', req.body.username);
+  res.redirect('/urls');
+});
+
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars = { username: req.cookies["username"] };
+  res.render("urls_new", templateVars);
 });
 
 app.post("/urls/:shortURL", (req, res) => {
@@ -54,7 +64,7 @@ app.post("/urls/:shortURL/delete", (req, res) => {
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: req.params.longURL };
+  const templateVars = { username: req.cookies["username"], shortURL: req.params.shortURL, longURL: req.params.longURL };
   res.render("urls_show", templateVars);
 });
 
